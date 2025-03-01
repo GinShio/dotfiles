9c2ddf09e829d4108d450b08c8a3f3d4e21c4708
GinShio <ginshio78@gmail.com>
2025-03-02 00:03:18 +0800

    git: Update templates

--- dotfiles/git/templates/hooks/check/format.sh
+++ dotfiles/git/templates/hooks/check/format.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! [ -e $GIT_TOPLEVEL_DIR/.clang-format ]; then
+    exit 0
+fi
+
 count=0
 for ctx in `git diff --cached --name-only --diff-filter=ACM |git check-attr --stdin text |tr -d ' '`; do
     IFS=':' read file attr info <<< "$ctx"
@@ -7,14 +11,12 @@ for ctx in `git diff --cached --name-only --diff-filter=ACM |git check-attr --st
         continue
     fi
     case "${file##*.}" in
-        c|h|inc) # C files
-            clang-format --Wno-error=unknown --style='file:$GIT_TOPLEVEL_DIR/.clang-format-c' -i "$file"
-            ;;
+        c|h|inc)           ;& # C files
         hh|hpp|hxx|h\+\+)  ;& # C++ header files
         cc|cpp|cxx|c\+\+)  ;& # C++ source files
         icc|ipp|ixx|i\+\+) ;& # C++ macro / template files
         tcc|tpp|txx|t\+\+)    # C++ template files
-            clang-format --Wno-error=unknown --style='file:$GIT_TOPLEVEL_DIR/.clang-format-cpp' -i "$file"
+            clang-format --Wno-error=unknown -i "$file"
             ;;
         *)
             sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' -e 's/[[:space:]]*$//' "$file"
