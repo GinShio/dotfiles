38bb9dd7a464c1899d887e2b61ee78ef4cece9e2
GinShio <ginshio78@gmail.com>
2025-04-01 22:59:03 +0800

    scripts: Fix typos

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 source $(dirname $0)/common/common.sh
-bash $DOTFILES_ROOT_PATH/scripts/daily/updatd-system.sh
+bash $DOTFILES_ROOT_PATH/scripts/daily/update-system.sh
 bash $DOTFILES_ROOT_PATH/scripts/daily/update-hosts.sh
 
 now_timestamps=$(date +%s)
-bash $DOTFILES_ROOT_PATH/scripts/daily/updatd-projects.sh
+bash $DOTFILES_ROOT_PATH/scripts/daily/update-projects.sh
 bash $DOTFILES_ROOT_PATH/scripts/daily/test-wrapper.sh $now_timestamps
 
 #systemctl reboot
