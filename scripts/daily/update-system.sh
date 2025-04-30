2c25e114255094cda692a31fdf8bd880072a9194
GinShio <ginshio78@gmail.com>
2025-05-01 01:54:35 +0800

    scripts(daily): Better proxy

--- scripts/daily/update-system.sh
+++ scripts/daily/update-system.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-trap "sudo -k" EXIT
+trap "sudo -k; source $(dirname ${BASH_SOURCE[0]})/../common/unproxy.sh" EXIT
 source $(dirname ${BASH_SOURCE[0]})/../common/common.sh
 source $(dirname ${BASH_SOURCE[0]})/../common/proxy.sh
 sudo -Sv <<<$ROOT_PASSPHRASE
 sudo -E zypper ref
 sudo -Sv <<<$ROOT_PASSPHRASE
-zypper lr |sed '1,4d' |awk '{print $3}' |grep 'openSUSE:' |xargs -I@ sudo zypper up -y --repo @
-source $(dirname ${BASH_SOURCE[0]})/../common/unproxy.sh
+zypper lr |sed '1,4d' |awk '{print $3}' |grep 'openSUSE:' |xargs -I@ sudo -E zypper up -y --repo @
 sudo -Sv <<<$ROOT_PASSPHRASE
-sudo -E zypper up -y
+sudo zypper up -y
