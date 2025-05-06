b25cf5a28e9a09ce7c5f2a4e3b8c1d21ab2798c0
GinShio <ginshio78@gmail.com>
2025-05-06 11:26:09 +0800

    home(autostart): fix order

--- dotfiles/xdg-autostart/develop.sh
+++ dotfiles/xdg-autostart/develop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
+bash {{@@ _dotdrop_workdir @@}}/scripts/autostart/mount_samba.sh
 bash {{@@ _dotdrop_workdir @@}}/scripts/autostart/copy_gpu_tests.sh
 bash {{@@ _dotdrop_workdir @@}}/scripts/autostart/create_terminal.sh
-bash {{@@ _dotdrop_workdir @@}}/scripts/autostart/mount_samba.sh
