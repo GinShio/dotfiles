7635e82fbfb0054c8cd8a5c301db03f3269223b0
GinShio <ginshio78@gmail.com>
2025-05-11 20:36:15 +0800

    scripts(daily): Using proxy for update projects

--- scripts/daily/update-projects.sh
+++ scripts/daily/update-projects.sh
@@ -9,7 +9,14 @@ if [ -e $HOME/Projects/amdvlk ] && [ -e $HOME/Projects/Builder ]; then
         python3 $HOME/Projects/Builder/scripts/main.py --loglevel=info -pxgl build --debug --tool
     fi
 fi
-bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=mesa
-trap "source $(dirname ${BASH_SOURCE[0]})/../common/unproxy.sh" EXIT
-source $(dirname ${BASH_SOURCE[0]})/../common/proxy.sh
-bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=llvm
+
+project_list=(mesa llvm)
+for project in ${project_list[@]}; do
+    source $(dirname ${BASH_SOURCE[0]})/../common/proxy.sh
+    bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=$project --skipbuild
+    status=$?
+    source $(dirname ${BASH_SOURCE[0]})/../common/unproxy.sh
+    if [ $status -eq 0 ]; then
+        bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=$project --skippull
+    fi
+done
