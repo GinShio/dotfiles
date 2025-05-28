db3c72a987ee57c2735f57fd99217ee4f83e09f8
GinShio <ginshio78@gmail.com>
2025-05-28 23:35:35 +0800

    scripts(common): Fix incorrectly set gpu profile

--- scripts/common/amdgpu-profile.sh
+++ scripts/common/amdgpu-profile.sh
@@ -2,9 +2,13 @@
 
 PROFILE=${1:-auto}
 
+source $(dirname ${BASH_SOURCE[0]})/common.sh
+trap "sudo -k" EXIT
+sudo -Sv <<<$ROOT_PASSPHRASE
+
 for device in $(ls -1 -d /sys/module/amdgpu/drivers/pci:amdgpu/*/); do
     if ! [ -e $device/device ]; then
         continue
     fi
-    echo $PROFILE >$device/power_dpm_force_performance_level
+    echo $PROFILE |sudo tee $device/power_dpm_force_performance_level
 done
