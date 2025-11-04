c43aa402d4ed98a5a566d39fdac0df4c6bd1825d
GinShio <ginshio78@gmail.com>
2025-11-04 23:26:12 +0800

    dotfiles: Refactor for script builder

--- dotfiles/fish/functions/__ginshio_copy-graphisc-testcases.fish
+++ dotfiles/fish/functions/__ginshio_copy-graphisc-testcases.fish
@@ -1,12 +1,12 @@
 function copy-graphics-testcase
     argparse deqp piglit tool vkd3d -- $argv
     or return
-    set -fx PROJECT_DIR $HOME/Projects/khronos3d
+    set -fx PROJECT_DIR $HOME/Projects
     set -fx RUNNER_DIR $XDG_RUNTIME_DIR/runner
 
     if set -ql _flag_deqp
-        and test -e $PROJECT_DIR/deqp
-        set -lx DEQP_SRCDIR $PROJECT_DIR/deqp
+        and test -e $PROJECT_DIR/khronos/deqp
+        set -lx DEQP_SRCDIR $PROJECT_DIR/khronos/deqp
         set -lx DEQP_DSTDIR $RUNNER_DIR/deqp
         set -lx DEQP_EXCLUDE $DEQP_DSTDIR/vk-exclude.txt
         if not test -e $DEQP_EXCLUDE
@@ -17,11 +17,11 @@ query-pool.txt
 video.txt
 wsi.txt" >$DEQP_EXCLUDE
         end
-        rsync $DEQP_SRCDIR/_build/external/vulkancts/modules/vulkan/Release/deqp-vk $DEQP_DSTDIR
+        rsync $DEQP_SRCDIR/_build/main/external/vulkancts/modules/vulkan/Release/deqp-vk $DEQP_DSTDIR
         rsync -rR $DEQP_SRCDIR/external/vulkancts/data/./vulkan $DEQP_DSTDIR
         rsync -rR --exclude-from=$DEQP_DSTDIR/vk-exclude.txt $DEQP_SRCDIR/external/vulkancts/mustpass/main/./vk-default $DEQP_DSTDIR/mustpass
-        rsync $DEQP_SRCDIR/_build/external/openglcts/modules/Release/glcts $DEQP_DSTDIR
-        rsync -rR $DEQP_SRCDIR/_build/external/openglcts/modules/./gles{2,3,31}/{data,shaders} $DEQP_DSTDIR
+        rsync $DEQP_SRCDIR/_build/main/external/openglcts/modules/Release/glcts $DEQP_DSTDIR
+        rsync -rR $DEQP_SRCDIR/_build/main/external/openglcts/modules/./gles{2,3,31}/{data,shaders} $DEQP_DSTDIR
         rsync -rR $DEQP_SRCDIR/external/graphicsfuzz/data/./gles3/graphicsfuzz/ $DEQP_DSTDIR
         rsync -rR --exclude='mustpass' $DEQP_SRCDIR/external/openglcts/data/./gl_cts $DEQP_DSTDIR
         rsync -rR --exclude='src' -f'- *-gtf-main.txt' $DEQP_SRCDIR/external/openglcts/data/gl_cts/data/mustpass/./gl/khronos_mustpass{,_single}/main/*.txt $DEQP_DSTDIR/mustpass
@@ -31,24 +31,24 @@ wsi.txt" >$DEQP_EXCLUDE
     end
 
     if set -ql _flag_piglit
-        and test -e $PROJECT_DIR/piglit
-        set -lx PIGLIT_SRCDIR $PROJECT_DIR/piglit
+        and test -e $PROJECT_DIR/fd.o/piglit
+        set -lx PIGLIT_SRCDIR $PROJECT_DIR/fd.o/piglit
         set -lx PIGLIT_DSTDIR $RUNNER_DIR/piglit
-        rsync -rR $PIGLIT_SRCDIR/_build/./bin $PIGLIT_DSTDIR
-        rsync -l $PIGLIT_SRCDIR/_build/lib/Release/* $PIGLIT_DSTDIR/lib
+        rsync -rR $PIGLIT_SRCDIR/_build/main/./bin $PIGLIT_DSTDIR
+        rsync -l $PIGLIT_SRCDIR/_build/main/lib/Release/* $PIGLIT_DSTDIR/lib
         rsync -rR $PIGLIT_SRCDIR/./{framework,templates} $PIGLIT_DSTDIR
-        rsync -rR $PIGLIT_SRCDIR/_build/./tests/*.xml.gz $PIGLIT_DSTDIR
+        rsync -rR $PIGLIT_SRCDIR/_build/main/./tests/*.xml.gz $PIGLIT_DSTDIR
         rsync -mrR -f'- *.[chao]' -f'- *.[ch]pp' -f'- *[Cc][Mm]ake*' $PIGLIT_SRCDIR/./tests $PIGLIT_DSTDIR
         rsync -rR $PIGLIT_SRCDIR/./generated_tests/**/*.inc $PIGLIT_DSTDIR
-        rsync -mrR -f'- *.[chao]' -f'- *.[ch]pp' -f'- *[Cc][Mm]ake*' -f'- *.list' $PIGLIT_SRCDIR/_build/./generated_tests $PIGLIT_DSTDIR
+        rsync -mrR -f'- *.[chao]' -f'- *.[ch]pp' -f'- *[Cc][Mm]ake*' -f'- *.list' $PIGLIT_SRCDIR/_build/main/./generated_tests $PIGLIT_DSTDIR
         for elf in $PIGLIT_DSTDIR/bin/* $PIGLIT_DSTDIR/lib/*.so
             patchelf --set-rpath "$PIGLIT_DSTDIR/lib" $elf
         end
     end
 
     if set -ql _flag_tool
-        and test -e $PROJECT_DIR/runner
-        rsync $PROJECT_DIR/runner/_build/release/{deqp,piglit}-runner $RUNNER_DIR
+        and test -e $PROJECT_DIR/fd.o/runner
+        rsync $PROJECT_DIR/fd.o/runner/_build/main/release/{deqp,piglit}-runner $RUNNER_DIR
     end
 
     if set -ql _flag_vkd3d
--- dotfiles/systemd/scripts/daily/update-projects.sh
+++ dotfiles/systemd/scripts/daily/update-projects.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
 source {{@@ _dotdrop_workdir @@}}/scripts/common/common.sh
-
-if [ -e $HOME/Projects/amdvlk ] && [ -e $HOME/Projects/Builder ]; then
-    python3 $HOME/Projects/Builder/scripts/main.py --loglevel=info -pxgl monorepo
-    if [ $? -eq 0 ]; then
-        python3 $HOME/Projects/Builder/scripts/main.py --loglevel=info -pxgl build --release
-        python3 $HOME/Projects/Builder/scripts/main.py --loglevel=info -pxgl build --debug --tool
-    fi
-fi
-
 source {{@@ _dotdrop_workdir @@}}/scripts/common/proxy.sh
 trap "source {{@@ _dotdrop_workdir @@}}/scripts/common/unproxy.sh" EXIT
-project_list=(mesa llvm)
-for project in ${project_list[@]}; do
-    bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=$project --skipbuild
-    if [ $? -eq 0 ]; then
-        bash $DOTFILES_ROOT_PATH/scripts/projects.sh --project=$project --skippull
-    fi
-done
+BUILDER_SCRIPT_DIR=$HOME/Projects/builder
+exisit_projects=( $(python3 $BUILDER_SCRIPT_DIR/builder.py list --no-submodule |sed '/^Warning:/d' |tail -n +3 |awk '{if ($4 != "<missing>") print $2}') )
+
+build_projects() {
+    local -n extra_args="$1"
+    local -n projects="$2"
+    for project in ${projects[@]}; do
+        if ! [[ "${exisit_projects[*]}" =~ "$project" ]]; then
+            continue
+        fi
+        python3 $BUILDER_SCRIPT_DIR/builder.py update $project
+        if [ $? -eq 0 ]; then
+            eval "python3 $BUILDER_SCRIPT_DIR/builder.py build $project --build-type Release ${extra_args[@]}"
+        fi
+    done
+}
+
+{%@@ if profile == "khronos3d" @@%}
+work_extra_args=(--install)
+work_projects=(amdvlk)
+build_projects work_extra_args work_projects
+{%@@ endif @@%}
+
+installable_extra_args=(--install-dir $HOME/.local --install)
+installable_projects=(mesa spirv-tools slang)
+build_projects installable_extra_args installable_projects
+
+common_extra_args=()
+common_projects=(dxc llvm)
+build_projects common_extra_args common_projects
