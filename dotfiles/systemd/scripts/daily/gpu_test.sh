688c9b8153108a8842a49e79f38dcacd4144d78c
GinShio <ginshio78@gmail.com>
2025-11-07 03:56:04 +0800

    Rename variables

--- dotfiles/systemd/scripts/daily/gpu_test.sh
+++ dotfiles/systemd/scripts/daily/gpu_test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 source {{@@ _dotdrop_workdir @@}}/scripts/common/common.sh
-XDG_RUNTIME_DIR=/run/user/$(id -u $USER)
-RUNNER_DIR=$XDG_RUNTIME_DIR/runner
 if [ -z {{@@ testing.result_dir @@}} ]
 then TEST_RESULT_DIR={{@@ testing.baseline_dir @@}}
 else TEST_RESULT_DIR={{@@ testing.result_dir @@}}
