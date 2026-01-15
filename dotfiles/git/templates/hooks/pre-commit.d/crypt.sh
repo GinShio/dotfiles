2b6a037d70f3f9f80478748323d45fcc18377335
GinShio <ginshio78@gmail.com>
2026-01-15 22:24:01 +0800

    home(git): Migrate hook scripts to other location

--- dotfiles/git/templates/hooks/pre-commit.d/crypt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Transcrypt pre-commit hook: fail if secret file in staging lacks the magic prefix "Salted" in B64
-CRYPT_DIR=$(git config transcrypt.crypt-dir 2>/dev/null || printf '%s/crypt' "${GIT_COMMON_DIR}")
-if [ -e "${CRYPT_DIR}/transcrypt" ]; then
-    "${CRYPT_DIR}/transcrypt" pre_commit
-fi
