1696b9305896ee0220111b014b61dc50e1678ad7
GinShio <ginshio78@gmail.com>
2026-01-29 03:18:09 +0800

    home(fish): Migrate set-git-urls to scripts

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,6 +6,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set --universal fish_greeting
     source {{@@ _dotfile_abs_dst @@}}/create_abbrs.fish
-    source {{@@ _dotfile_abs_dst @@}}/functions/__ginshio_set-git-urls.fish
     source {{@@ _dotfile_abs_dst @@}}/functions/__ginshio_zswap-statistics.fish
 end
