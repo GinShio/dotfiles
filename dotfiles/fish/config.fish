48273e84148dd0e238687f858765ca3319b09771
GinShio <ginshio78@gmail.com>
2026-02-13 09:06:19 +0800

    home(fish): Set envvars from systemd-environment

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,5 +6,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set --universal fish_greeting
     source {{@@ _dotfile_abs_dst @@}}/create_abbrs.fish
+    source {{@@ _dotfile_abs_dst @@}}/functions/__ginshio_login_envvars.fish
     source {{@@ _dotfile_abs_dst @@}}/functions/__ginshio_zswap-statistics.fish
 end
